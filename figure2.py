195d8e5e543e543aef512207af25de412d4fd2b2
MartaAneta <martaostrowska114@gmail.com>
2023-05-24 18:50:41 +0200

    krok3

--- figure2.py
+++ figure2.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array(["A", "B", "C", "D"])
-y = np.array([3, 8, 1, 10])
+# x = np.array(["A", "B", "C", "D"])
+# y = np.array([3, 8, 1, 10])
+#
+# plt.barh(x, y)
+y = np.array([35, 25, 25, 15])
 
-plt.barh(x, y)
+plt.pie(y)
+plt.show()
 plt.show()
