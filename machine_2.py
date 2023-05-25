5911b4f1fc3b1d46d26c4ecea8eab99084feafbb
MartaAneta <martaostrowska114@gmail.com>
2023-05-25 19:33:06 +0200

    krok6

--- machine_2.py
+++ machine_2.py
@@ -1,16 +1,28 @@
-from scipy import stats
+# from scipy import stats
+#
+# x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+# y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+#
+# slope, intercept, r, p, std_err = stats.linregress(x, y)
+#
+# def myfunc(x):
+#   return slope * x + intercept
+#
+# speed = myfunc(10)
+#
+# print(speed)
+import numpy
+import matplotlib.pyplot as plt
 
-x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
-y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
 
-slope, intercept, r, p, std_err = stats.linregress(x, y)
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
 
-def myfunc(x):
-  return slope * x + intercept
+myline = numpy.linspace(1, 22, 100)
 
-speed = myfunc(10)
-
-print(speed)
-
+plt.scatter(x, y)
+plt.plot(myline, mymodel(myline))
+plt.show()
 
 
