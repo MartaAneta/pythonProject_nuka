923eb18dda72d41bf5fba9519453545fd1f6859b
MartaAneta <martaostrowska114@gmail.com>
2023-05-27 12:19:18 +0200

    krok12

--- MY_SQL.py
+++ MY_SQL.py
@@ -12,9 +12,28 @@ mycursor = mydb.cursor()
 
 #mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
 sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("Marta", "Highway 21")
-mycursor.execute(sql, val)
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)
 
 mydb.commit()
+mycursor.execute("SELECT * FROM customers")
 
-print(mycursor.rowcount, "record inserted.")
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
