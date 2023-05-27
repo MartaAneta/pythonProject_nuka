57d1b494ea460e44c446c8903163957ba6c6e33f
MartaAneta <martaostrowska114@gmail.com>
2023-05-27 11:23:31 +0200

    krok10

--- MY_SQL.py
+++ MY_SQL.py
@@ -1,13 +1,13 @@
 import mysql.connector
 
+
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password=""
+  password="",
+  database="mydatabase"
 )
+
 mycursor = mydb.cursor()
 
-mycursor.execute("SHOW DATABASES")
-
-for x in mycursor:
-  print(x)
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
