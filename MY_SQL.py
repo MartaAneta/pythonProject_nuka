3f50bb33b484a880a93d3ddd4784898701f26f7f
MartaAneta <martaostrowska114@gmail.com>
2023-05-27 11:39:46 +0200

    krok11

--- MY_SQL.py
+++ MY_SQL.py
@@ -10,4 +10,11 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+#mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("Marta", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
